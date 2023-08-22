7526e0db195c350cb43358263c06a7a04b2aff69
Mirah <you@example.com>
2023-08-22 14:14:23 +0200

    HotelDeVille.h && upd gitignore

---
+++ include/hotelDeVille.h
@@ -0,0 +1,13 @@
+# include "village.h"
+
+#ifndef hotelDeVille_H
+#define hotelDeVille_H
+    
+typedef struct S_hotelDeVille
+{
+    int villageois;
+    int prodVillageois;
+    int villageoisMax;
+} S_hotelDeVille;
+
+#endif
