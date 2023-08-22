97f29dc8b91cc9896f5300c3c0959579cc564b22
lionel <lpds.desousa@gmail.com>
2023-08-22 14:29:09 +0200

    basic village

--- include/village.h
+++ include/village.h
@@ -7,6 +7,13 @@
 // # include "mine.h"
 
     // Mine : production or/jour
+    typedef struct Village
+    {
+        int jour;
+        S_resVillage *resVillage;
+        S_hotelDeVille *HDV;
+    } S_Village  ;
+
     int prodOr(int or);
 
 #endif
