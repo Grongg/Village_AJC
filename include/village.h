5a9eacdeb67c3705185e7b9547b68ff36ebc8b51
Mirah <you@example.com>
2023-08-22 14:22:17 +0200

    add village.h

---
+++ include/village.h
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+#ifndef village_H
+#define village_H
+    
+# include "hotelDeVille.h"
+# include "mine.h"
+
+    // Mine : production or/jour
+    int prodOr(int or);
+
+#endif
